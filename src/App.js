b146ce50aa8f25b902817adef408588bd7a5a9aa
Rose Aranaga-Hill <rosearanagahill@gmail.com>
2021-10-01 11:53:54 -0400

    push amplify api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
 
+// Amplify imports
+import Amplify from '@aws-amplify/core'
+import awsExports from './aws-exports'
+Amplify.configure(awsExports)
+
 function App() {
   return (
     <div className="App">
