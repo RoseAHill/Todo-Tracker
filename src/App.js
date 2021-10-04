ffaec496240385ba28b2192296603478284de464
Rose Aranaga-Hill <rosearanagahill@gmail.com>
2021-10-04 17:37:24 -0400

    add base styling to app

--- src/App.js
+++ src/App.js
@@ -1,113 +1,100 @@
 /* src/App.js */
+import './App.css'
 import React, { useEffect, useState } from 'react'
-import Amplify, { API, graphqlOperation } from 'aws-amplify'
+import Amplify from 'aws-amplify'
 import { withAuthenticator } from '@aws-amplify/ui-react'
-import { createTodo } from './graphql/mutations'
-import { listTodos } from './graphql/queries'
+import {
+  fetchTodosByDate as byDate,
+  fetchTodosByStatus as byStatus,
+  fetchTodosByTitle as byTitle,
+  sendTodo,
+  initialState,
+  readableStatus
+} from './services/todoService'
 
-import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
-
-const initialState = {
-  title: '',
-  description: '',
-  status: 'NOTSTARTED',
-  dueDate: '',
-}
-
-const readableStatus = {
-  "NOTSTARTED": "Not Started",
-  "INPROGRESS": "In-progress",
-  "COMPLETE": "Complete",
-  "ONHOLD": "On Hold"
-}
+import awsExports from "./aws-exports"
+Amplify.configure(awsExports)
 
 const App = () => {
   const [formState, setFormState] = useState(initialState)
   const [todos, setTodos] = useState([])
-
+  const [sortDirection, setSortDirection] = useState('ASC')
+  
   useEffect(() => {
-    fetchTodos()
+    fetchTodos(byDate, 'ASC')
   }, [])
-
-  function setInput(key, value) {
+  
+  const setInput = (key, value) => {
     setFormState({ ...formState, [key]: value })
   }
-
-  async function fetchTodos() {
+  
+  const fetchTodos = async (sortType, direction) => {
     try {
-      const todoData = await API.graphql(graphqlOperation(listTodos))
-      const todos = todoData.data.listTodos.items
+      const todos = await sortType(direction)
       setTodos(todos)
-    } catch (err) { console.log('error fetching todos') }
+    } catch (err) { console.error('error fetching todos', err) }
   }
-
-  async function addTodo() {
+  
+  const addTodo = async () => {
     try {
       if (!formState.title || !formState.description) return
       const todo = { ...formState }
       setTodos([...todos, todo])
       setFormState(initialState)
-      await API.graphql(graphqlOperation(createTodo, {input: todo}))
+      await sendTodo(todo)
     } catch (err) {
       console.log('error creating todo:', err)
     }
   }
 
+  
   return (
-    <div style={styles.container}>
+    <div className='container'>
       <h2>Task Tracker</h2>
       <input
         required
         name="title"
         onChange={event => setInput('title', event.target.value)}
-        style={styles.input}
+        className='input'
         value={formState.title}
         placeholder="Title"
       />
       <input
         name="description"
         onChange={event => setInput('description', event.target.value)}
-        style={styles.input}
+        className='input'
         value={formState.description}
         placeholder="Description"
       />
       <input
+        required
         type="date"
         name="dueDate"
+        className='input'
         onChange={(e) => setInput('dueDate', e.target.value)}
         value={formState.dueDate}
       />
-      <select defaultValue="NOTSTARTED" name="status" onChange={e => setInput('status', e.target.value)}>
+      <select required className='input' defaultValue="NOTSTARTED" name="status" onChange={e => setInput('status', e.target.value)}>
         <option value="NOTSTARTED">Not Started</option>
         <option value="INPROGRESS">In Progress</option>
         <option value="COMPLETE">Complete</option>
         <option value="ONHOLD">On Hold</option>
       </select>
-      <button style={styles.button} onClick={() => addTodo()}>Create Todo</button>
+      <button className='button' onClick={() => addTodo()}>Create Todo</button>
       {
         todos.map((todo, index) => (
-          <div key={todo.id ? todo.id : index} style={styles.todo}>
-            <p style={styles.todoName}>{todo.name}</p>
-            <p style={styles.todoDescription}>{todo.description}</p>
+          <div key={todo.id ? todo.id : index} className='todo'>
+            <h2 className='todo-title'>{todo.title}</h2>
+            <p className='todo-description'>{todo.description}</p>
             <div className="todo-details">
               <p>Due: {todo.dueDate || "N/A"}</p>
-              <p>Status: {todo.status ? readableStatus[todo.status] : "[Not Assigned]"}</p>
+              <h3 className={todo.status}>{todo.status ? readableStatus[todo.status] : "[Not Assigned]"}</h3>
             </div>
           </div>
         ))
       }
     </div>
   )
-}
-
-const styles = {
-  container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
-  todo: {  marginBottom: 15 },
-  input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
-  todoName: { fontSize: 20, fontWeight: 'bold' },
-  todoDescription: { marginBottom: 0 },
-  button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
 }
 
 export default withAuthenticator(App)
