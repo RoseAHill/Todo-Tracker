e540edb63914f8e5b0526748238296769fbd9edb
Rose Aranaga-Hill <rosearanagahill@gmail.com>
2021-10-04 15:09:38 -0400

    rolledback restructures to simplify process

--- src/App.js
+++ src/App.js
@@ -1,33 +1,112 @@
-import React, { useState, useEffect } from 'react'
-import './App.css';
-import FormHeader from './components/Form/FormHeader'
-import List from './components/Display/List'
+/* src/App.js */
+import React, { useEffect, useState } from 'react'
+import Amplify, { API, graphqlOperation } from 'aws-amplify'
+import { createTodo } from './graphql/mutations'
+import { listTodos } from './graphql/queries'
 
-// api calls are stored in a service file
-import { getTodoList } from "./services/todoServices"
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+const initialState = {
+  title: '',
+  description: '',
+  status: 'NOTSTARTED',
+  dueDate: '',
+}
+
+const readableStatus = {
+  "NOTSTARTED": "Not Started",
+  "INPROGRESS": "In-progress",
+  "COMPLETE": "Complete",
+  "ONHOLD": "On Hold"
+}
 
 const App = () => {
-  const [todos, setTodos] = useState([]);
+  const [formState, setFormState] = useState(initialState)
+  const [todos, setTodos] = useState([])
 
   useEffect(() => {
-    const fetchTodos = async () => {
-      try {
-        const todoList = await getTodoList()
-        setTodos(todoList)
-      } catch (err) { console.error("error fetching todos", err) }
-    }
     fetchTodos()
   }, [])
 
+  function setInput(key, value) {
+    setFormState({ ...formState, [key]: value })
+  }
+
+  async function fetchTodos() {
+    try {
+      const todoData = await API.graphql(graphqlOperation(listTodos))
+      const todos = todoData.data.listTodos.items
+      setTodos(todos)
+    } catch (err) { console.log('error fetching todos') }
+  }
+
+  async function addTodo() {
+    try {
+      if (!formState.title || !formState.description) return
+      const todo = { ...formState }
+      setTodos([...todos, todo])
+      setFormState(initialState)
+      await API.graphql(graphqlOperation(createTodo, {input: todo}))
+    } catch (err) {
+      console.log('error creating todo:', err)
+    }
+  }
+
   return (
-    <div className="App">
-      <header>
-        <h1>Task Tracker</h1>
-      </header>
-      <FormHeader todos={todos} setTodos={setTodos} />
-      <List todos={todos} />
+    <div style={styles.container}>
+      <h2>Task Tracker</h2>
+      <input
+        required
+        name="title"
+        onChange={event => setInput('title', event.target.value)}
+        style={styles.input}
+        value={formState.title}
+        placeholder="Title"
+      />
+      <input
+        name="description"
+        onChange={event => setInput('description', event.target.value)}
+        style={styles.input}
+        value={formState.description}
+        placeholder="Description"
+      />
+      <input
+        type="date"
+        name="dueDate"
+        onChange={(e) => setInput('dueDate', e.target.value)}
+        value={formState.dueDate}
+      />
+      <select defaultValue="NOTSTARTED" name="status" onChange={e => setInput('status', e.target.value)}>
+        <option value="NOTSTARTED">Not Started</option>
+        <option value="INPROGRESS">In Progress</option>
+        <option value="COMPLETE">Complete</option>
+        <option value="ONHOLD">On Hold</option>
+      </select>
+      <button style={styles.button} onClick={() => addTodo()}>Create Todo</button>
+      {
+        todos.map((todo, index) => (
+          <div key={todo.id ? todo.id : index} style={styles.todo}>
+            <p style={styles.todoName}>{todo.name}</p>
+            <p style={styles.todoDescription}>{todo.description}</p>
+            <div className="todo-details">
+              <p>Due: {todo.dueDate || "N/A"}</p>
+              <p>Status: {todo.status ? readableStatus[todo.status] : "[Not Assigned]"}</p>
+            </div>
+          </div>
+        ))
+      }
     </div>
-  );
+  )
 }
 
-export default App;
+const styles = {
+  container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
+  todo: {  marginBottom: 15 },
+  input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
+  todoName: { fontSize: 20, fontWeight: 'bold' },
+  todoDescription: { marginBottom: 0 },
+  button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
+}
+
+export default App
