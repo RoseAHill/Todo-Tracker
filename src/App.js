a6b5474fcff6cf391b3de35cb2c04328cf18e9b3
Rose Aranaga-Hill <rosearanagahill@gmail.com>
2021-10-04 15:23:18 -0400

    add auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 /* src/App.js */
 import React, { useEffect, useState } from 'react'
 import Amplify, { API, graphqlOperation } from 'aws-amplify'
+import { withAuthenticator } from '@aws-amplify/ui-react'
 import { createTodo } from './graphql/mutations'
 import { listTodos } from './graphql/queries'
 
@@ -109,4 +110,4 @@ const styles = {
   button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
 }
 
-export default App
+export default withAuthenticator(App)
