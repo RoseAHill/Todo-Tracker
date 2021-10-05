f83fe1e4b0fef7b93237d69fa7a4646a0b9a8b42
Rose Aranaga-Hill <rosearanagahill@gmail.com>
2021-10-04 21:07:42 -0400

    add edit  button

--- src/components/list/Todo.jsx
+++ src/components/list/Todo.jsx
@@ -1,16 +1,33 @@
 import React from 'react'
+import { Dropdown } from 'react-bootstrap'
 
-const Todo = ({ title, description,dueDate, status, readableStatus }) => {
+const Todo = ({ todoId, title, description, dueDate, status, readableStatus }) => {
   const today = new Date()
   const dueWhen = new Date(dueDate)
 
+  const editMenu = (
+    <Dropdown>
+      <Dropdown.Toggle variant="success-outline" id={`edit-${todoId}`}>
+        <i className="far fa-edit" />
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+        <Dropdown.Item variant="danger" href="#/action-3">Something else</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  )
+
   return (
     <div className="todo">
-      <h2 className='todo-title'>{title}</h2>
+      <div className="todo-top">
+        <h2 className='todo-title'>{title}</h2>
+        { editMenu }
+      </div>
       <p className='todo-description'>{description}</p>
       <div className="todo-details">
         <p className={today >= dueWhen && status !== 'COMPLETE' ? `due` : `${status}`}>Due: {dueDate}</p>
-        <h3 className={status}>{readableStatus}</h3>
+        <p className={`status ${status}`}>{readableStatus}</p>
       </div>
     </div>
   )
